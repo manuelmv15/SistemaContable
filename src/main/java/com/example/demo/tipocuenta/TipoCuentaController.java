bd61cb4fa469e363cb4488099a004e359de76b48
kristiankovic <vv23011@ues.edu.sv>
2025-10-09 19:14:33 -0600

    style: se agrego el diseño del dashboard del contador

--- src/main/java/com/example/demo/tipocuenta/TipoCuentaController.java
+++ src/main/java/com/example/demo/tipocuenta/TipoCuentaController.java
@@ -15,23 +15,31 @@ public class TipoCuentaController {
 
     public TipoCuentaController(TipoCuentaService service) { this.service = service; }
 
+    //obtiene la lista de todos los tipos de cuentas
     @GetMapping
     public List<TipoCuenta> listar() { return service.listar(); }
 
+    //obtiene un tipo de cuenta referenciado por su id
     @GetMapping("/{id}")
     public TipoCuenta get(@PathVariable Long id) { return service.obtener(id); }
 
+    //crea un nuevo tipo de cuenta y retorna la entidad creada con su id
     @PostMapping
     public ResponseEntity<TipoCuenta> crear(@Validated @RequestBody TipoCuenta dto) {
         var saved = service.crear(dto);
         return ResponseEntity.created(URI.create("/api/tipos-cuenta/" + saved.getIdTipoCuenta())).body(saved);
     }
 
+    //actualiza un tipo de cuenta en existencia
+    //id-> identicador del tipo de cuenta que se va actualizar
+    //dto-> datos actualizados del tipo de cuenta
     @PutMapping("/{id}")
     public TipoCuenta actualizar(@PathVariable Long id, @Validated @RequestBody TipoCuenta dto) {
         return service.actualizar(id, dto);
     }
 
+    //elimina un tipo de cuenta en existencia
+    //id-> identicador del tipo de cuenta que se va eliminar
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         service.eliminar(id);
