82f1dc33061c2e27d0b4d227efb3342c7b6ced7b
CesarZV23005 <zv23005@ues.edu.sv>
2025-11-21 21:01:09 -0600

    partida requestdto

--- src/main/java/com/example/demo/dto/PartidaRequestDTO.java
+++ src/main/java/com/example/demo/dto/PartidaRequestDTO.java
@@ -5,15 +5,12 @@ import java.util.List;
 public class PartidaRequestDTO {
     private String fecha;      // "YYYY-MM-DD"
     private String concepto;
-    private Long usuarioId;
     private List<DetallePartidaDTO> detalles;
 
     public String getFecha() { return fecha; }
     public void setFecha(String fecha) { this.fecha = fecha; }
     public String getConcepto() { return concepto; }
     public void setConcepto(String concepto) { this.concepto = concepto; }
-    public Long getUsuarioId() { return usuarioId; }
-    public void setUsuarioId(Long usuarioId) { this.usuarioId = usuarioId; }
     public List<DetallePartidaDTO> getDetalles() { return detalles; }
     public void setDetalles(List<DetallePartidaDTO> detalles) { this.detalles = detalles; }
 }
