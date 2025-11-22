6139d333125248db111c5311208b48da837e81a4
CesarZV23005 <zv23005@ues.edu.sv>
2025-11-21 20:53:45 -0600

    update partidaapicontroller

--- src/main/java/com/example/demo/apis/PartidaApiController.java
+++ src/main/java/com/example/demo/apis/PartidaApiController.java
@@ -4,26 +4,17 @@ import com.example.demo.dto.PartidaRequestDTO;
 import com.example.demo.partida.Partida;
 import com.example.demo.partida.PartidaService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/partidas")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class PartidaApiController {
 
     private final PartidaService partidaService;
 
-    @PostMapping("/registrar")
-    public ResponseEntity<?> registrar(@RequestBody PartidaRequestDTO dto) {
-        Partida p = partidaService.crearDesdeDTO(dto);
-        return ResponseEntity.ok(p);
-    }
-
-    // opcional: listar partidas
-    @GetMapping
-    public ResponseEntity<?> listar() {
-        return ResponseEntity.ok(partidaService);
+    @PostMapping("/crear")
+    public Partida crearPartida(@RequestBody PartidaRequestDTO dto) {
+        return partidaService.guardarPartida(dto);
     }
 }
